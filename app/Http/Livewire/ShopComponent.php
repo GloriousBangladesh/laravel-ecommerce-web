140fa716f6283125a2ec66b2214b5f765dad76c1
AbidHasan007 <ahmasum890@gmail.com>
2021-08-08 00:35:33 +0600

    second commit and first update

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -1,13 +1,16 @@
 <?php
 
 namespace App\Http\Livewire;
-
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ShopComponent extends Component
 {
+    
     public function render()
     {
-        return view('livewire.shop-component')->layout('layouts.base');
+        $products = Product::paginate(3);
+        return view('livewire.shop-component',['products'=> $products])->layout('layouts.base');
     }
 }
